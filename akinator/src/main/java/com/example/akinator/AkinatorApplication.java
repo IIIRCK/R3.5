6b35aa2d3764f2d0e669dafdae038ace315ac910
RACKHAM <juanmanuelmg003@gmail.com>
2023-11-06 15:33:22 +0100

    c1

---
+++ akinator/src/main/java/com/example/akinator/AkinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.akinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkinatorApplication.class, args);
+    }
+
+}
